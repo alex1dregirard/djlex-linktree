45bc814ae147f9a43e420c0f0d8eb489a20a3120
alexandre-girard <alexandre.girard@maif.fr>
2026-01-02 19:26:37 +0100

    remove Google Analytics integration and related code

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Gallery from './pages/Gallery';
 import About from './pages/About';
 import Booking from './pages/Booking';
-import { useEffect } from 'react';
-import { initGA, logPageView } from './pages/analytics';
 
 function App() {
-  useEffect(() => {
-    initGA("GTM-5DLHX4CM"); // Replace with your tracking ID
-  }, []);
-
-  const TrackPageView = () => {
-    const location = useLocation();
-    useEffect(() => {
-      logPageView();
-    }, [location]);
-
-    return null;
-  };
-
+  
   return (
     <BrowserRouter>
-      <TrackPageView />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/gallery" element={<Gallery />} />
