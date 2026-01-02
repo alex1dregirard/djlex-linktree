830f64ce32c9b6086ad16ae9491df65d55d008f4
alexandre-girard <alexandre.girard@maif.fr>
2026-01-02 09:43:04 +0100

    remove JoinCTA component and update LinkButton for Mixcloud

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Hero from './components/Hero';
 import LinkButton from './components/LinkButton';
-import JoinCTA from './components/JoinCTA';
 import Footer from './components/Footer';
 import './App.css';
 
@@ -10,10 +9,8 @@ function App() {
     <div className="frame">
       <Hero />
       <section className="links">
-        <LinkButton href="https://instagram.com/dj.lex.music.79">Instagram</LinkButton>
-        <LinkButton href="https://www.mixcloud.com/">Listen my mixes on Mixcloud</LinkButton>
+        <LinkButton href="https://www.mixcloud.com/dj_lex_79/">Listen my mixes on Mixcloud</LinkButton>
       </section>
-      <JoinCTA />
       <Footer />
     </div>
   );
