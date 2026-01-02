4efc596383f3effecb5b3ee340e6a7765dad5d4c
alexandre-girard <alexandre.girard@maif.fr>
2026-01-02 11:12:48 +0100

    update Hero component to enhance subtitle formatting and clarify music style

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,11 +9,12 @@ const Hero = () => {
         <h1 className="title">dj lex</h1>
         <p className="subtitle">
           <span className="emoji">🎧</span>
-          NOUVEAU MIX DISPONIBLE
+          {' '}NOUVEAU MIX DISPONIBLE{' '}
+          <span className="emoji">🎧</span>
         </p>
         <div className="subtitle subtitle--small">
-          Style : House / pop<br />
-          Ambiance : Années 90 / 2000<br />
+          Style : House / pop / commercial<br />
+          Ambiance : Opening - Années 90 / 2000<br />
           Link : <a href="https://www.mixcloud.com/dj_lex_79/mix-end-1990-2000-house-pop/" target="_blank" rel="noopener noreferrer">Mixcloud</a>
         </div>
         <InstagramLink />
