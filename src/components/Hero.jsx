cf93695db0ce08f66887608a1d70f360715e0ee2
alexandre-girard <alexandre.girard@maif.fr>
2026-01-10 19:03:28 +0100

    mix #2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,13 +9,13 @@ const Hero = () => {
         <h1 className="title">dj lex</h1>
         <p className="subtitle">
           <span className="emoji">🎧</span>
-          {' '}NOUVEAU MIX DISPONIBLE{' '}
+          {' '}NOUVEAU MIX DISPONIBLE - 10/01/2026{' '}
           <span className="emoji">🎧</span>
         </p>
         <div className="subtitle subtitle--small">
-          Style : House / pop / commercial<br />
-          Ambiance : Opening - Années 90 / 2000<br />
-          Link : <a href="https://www.mixcloud.com/dj_lex_79/mix-end-1990-2000-house-pop/" target="_blank" rel="noopener noreferrer">Mixcloud</a>
+          Style : pop - 80's / commercial<br />
+          Ambiance : Opening - Années 80<br />
+          Link : <a href="https://www.mixcloud.com/dj_lex_79/mix-80s-french-pop/" target="_blank" rel="noopener noreferrer">Mixcloud</a>
         </div>
         <InstagramLink />
       </div>
